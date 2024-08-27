9d8721af52088431e55ddaa5757daf19d36742fe
meremb <meremabart@gmail.com>
2024-08-27 10:36:01 +0200

    multiple rooms test

--- tests/test_simpleLoadModel.py
+++ tests/test_simpleLoadModel.py
@@ -1,6 +1,14 @@
-from simpleLoadModel import RoomLoadCalculator
+import json
 import pytest
 
+from simpleLoadModel import RoomLoadCalculator
+
+
+@pytest.fixture
+def load_test_data():
+    # Load the JSON test data
+    with open('test_building_data.json', 'r') as file:
+        return json.load(file)
 
 def test_roomload_calculator():
     floorArea = 10
@@ -13,7 +21,8 @@ def test_roomload_calculator():
     vSystem = 'C'
 
     result = 597.23
-    test1 = RoomLoadCalculator(floorArea, Uw, Uroof, Uground, vSystem, v50, Tin, Tout,
+    test1 = RoomLoadCalculator(floor_area=floorArea, uw=Uw, u_roof=Uroof, u_ground=Uground, v_system=vSystem, v50=v50,
+                               tin=Tin, tout=Tout,
                                neighbour_t=18,
                                lir=0.2,
                                heat_loss_area_estimation='fromFloorArea',
@@ -27,7 +36,8 @@ def test_roomload_calculator():
     assert result == pytest.approx(test1, rel=1e-2)
 
     result = 506.36
-    test2 = RoomLoadCalculator(floorArea, Uw, Uroof, Uground, v_system='D', v50=v50, tin=24, tout=Tout,
+    test2 = RoomLoadCalculator(floor_area=floorArea, uw=Uw, u_roof=Uroof, u_ground=Uground, v_system='D', v50=v50,
+                               tin=24, tout=Tout,
                                neighbour_t=18,
                                lir=0.2,
                                heat_loss_area_estimation='fromFloorArea',
@@ -41,7 +51,8 @@ def test_roomload_calculator():
     assert result == pytest.approx(test2, rel=1e-2)
 
     result = 576.69
-    test3 = RoomLoadCalculator(floorArea, Uw, Uroof, Uground, v_system='D', v50=v50, tin=24, tout=Tout,
+    test3 = RoomLoadCalculator(floor_area=floorArea, uw=Uw, u_roof=Uroof, u_ground=Uground, v_system='D', v50=v50,
+                               tin=24, tout=Tout,
                                neighbour_t=18,
                                lir=0.2,
                                heat_loss_area_estimation='fromExposedPerimeter',
@@ -53,3 +64,38 @@ def test_roomload_calculator():
                                room_type='Bathroom').compute()
 
     assert result == pytest.approx(test3, rel=1e-2)
+
+
+def test_multiple_room(load_test_data):
+    Uw = 0.24
+    Uroof = 0.24
+    Uground = 0.7
+    Un = 2
+    v50 = 1
+    Tout = -7
+    Tneighbour = 18
+    vSystem = 'C'
+
+    room_data = load_test_data["building"]["rooms"]
+    total_heat_loss = 0
+    for room in room_data:
+        calculator = RoomLoadCalculator(
+            floor_area=room["floor_area"],
+            uw=Uw,
+            u_roof=Uroof,
+            u_ground=Uground,
+            v_system=vSystem,
+            v50=v50,
+            tin=room["tin"],
+            tout=Tout,
+            neighbour_t=Tneighbour,
+            un=Un,
+            heat_loss_area_estimation='fromFloorArea',
+            on_ground=room["on_ground"],
+            under_roof=room["under_roof"],
+            room_type=room["room_type"],
+        )
+
+        result = calculator.compute()
+        total_heat_loss += float(result)
+    assert pytest.approx(total_heat_loss, rel=1e-2) == 4409.72
